bb34f598e5a077c38b2dcaa87d94e340be2a00c8
dgunther2001 <danielgunther2001@gmail.com>
2024-08-21 19:47:15 -0400

    Project Structure Initialization

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+#MIT License
+#Copyright (c) 2024 Daniel Gunther
+
+#For the full license text, see the LICENSE.txt file in the docs directory.
+#If LICENSE.txt is not included, this version of the source code is provided in breach of this license.
+
+#******************************************************
+
+FROM ubuntu:22.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    clang \
+    libc++-dev \
+    libc++abi-dev \
+    cmake \
+    llvm \
+    llvm-dev \
+    libllvm14 \
+    zlib1g-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+
+WORKDIR /
+
+COPY . .
+
+RUN mkdir build && cd build && \
+    cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS="-stdlib=libc++" .. && \
+    make
+
+CMD ["./build/driver"]
---
+++ src/driver.cpp
@@ -0,0 +1,40 @@
+/*
+MIT License
+Copyright (c) 2024 Daniel Gunther
+
+For the full license text, see the LICENSE.txt file in the docs directory.
+If LICENSE.txt is not included, this version of the source code is provided in breach of this license.
+*/
+
+#include "../include/ast/ast.h"
+#include "../include/codegen/codegen.h"
+#include "../include/lexer/lexer.h"
+#include "../include/parser/parser.h"
+#include "../include/type_checker/type_checker.h"
+#include "../include/utility/utility.h"
+
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+
+    std::cout << "My LLVM Driver is Working\n";
+
+    /* use later once input stream is generated
+    std::fstream file;
+    if (argc > 1) {
+        file.open(argv[1]);
+        if (!file) {
+            fprintf(stderr, "File not found.\n");
+            return 0;
+        }
+        input = &file;
+    } else {
+        fprintf(stderr, ">> "); // prime the inital token
+        input = &std::cin;
+    }
+
+    */
+
+    return 0;
+}
