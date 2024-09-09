fce5220cc73970840d74396b81e3d2591da0db4e
dgunther2001 <dgunther2001@users.noreply.github.com>
2024-09-09 17:31:17 +0000

    deploy: d6cd28eb68f2ab869bfe89e991df325edd2c6d1c

--- search/all_c.js
+++ search/all_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['roadmap_0',['Roadmap',['../index.html#autotoc_md1',1,'']]],
+  ['roadmap_0',['Roadmap',['../index.html#autotoc_md2',1,'']]],
   ['run_20pyroxene_1',['How do I run Pyroxene?',['../md_docs_2start.html',1,'']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['shells_0',['Zsh and Bash Shells',['../md_docs_2start.html#autotoc_md5',1,'']]],
-  ['start_2emd_1',['start.md',['../start_8md.html',1,'']]],
-  ['stored_5fvalues_2',['stored_values',['../namespacelexer.html#ac762e6415092515d1673f9ebd161bfcc',1,'lexer']]],
-  ['string_5fexpression_3',['string_expression',['../classast_1_1string__expression.html',1,'ast::string_expression'],['../classast_1_1string__expression.html#a8b04e515589756b25a853796cef7fcfb',1,'ast::string_expression::string_expression()']]],
-  ['string_5ftype_4',['string_type',['../namespaceast.html#a40c97931a3dcafde9769060c39123341ac4420b814242e6d8a8600492b52e5622',1,'ast']]],
-  ['string_5fvalue_5',['string_value',['../namespacelexer.html#ac6c312257777281ee94e7ff466400568',1,'lexer']]]
+  ['setup_20guide_0',['Setup Guide',['../index.html#autotoc_md1',1,'']]],
+  ['shells_1',['Zsh and Bash Shells',['../md_docs_2start.html#autotoc_md6',1,'']]],
+  ['start_2emd_2',['start.md',['../start_8md.html',1,'']]],
+  ['stored_5fvalues_3',['stored_values',['../namespacelexer.html#ac762e6415092515d1673f9ebd161bfcc',1,'lexer']]],
+  ['string_5fexpression_4',['string_expression',['../classast_1_1string__expression.html',1,'ast::string_expression'],['../classast_1_1string__expression.html#a8b04e515589756b25a853796cef7fcfb',1,'ast::string_expression::string_expression()']]],
+  ['string_5ftype_5',['string_type',['../namespaceast.html#a40c97931a3dcafde9769060c39123341ac4420b814242e6d8a8600492b52e5622',1,'ast']]],
+  ['string_5fvalue_6',['string_value',['../namespacelexer.html#ac6c312257777281ee94e7ff466400568',1,'lexer']]]
 ];
